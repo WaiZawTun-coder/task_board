41c9afec2e7bea20801476d94b1637ecff7a53ef
WaiZawTun-coder <waizawtun.coder@gmail.com>
2026-08-03 18:11:01 +0630

    testing

--- proxy.ts
+++ proxy.ts
@@ -5,6 +5,7 @@ import { ratelimit } from "./lib/rate-limit.lib";
 export async function proxy(request: NextRequest) {
   try {
     const ip = request.headers.get("x-forwarded-for") ?? "127.0.0.1";
+    console.log({ ip });
     const { success } = await ratelimit.limit(ip);
 
     if (!success) {
