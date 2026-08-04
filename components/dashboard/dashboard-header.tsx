10c8a148c5cfff48ee3f8577ffe570693686e1f9
WaiZawTun-coder <waizawtun.coder@gmail.com>
2026-08-04 14:44:03 +0630

    fix: logo

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -110,8 +110,8 @@ const DashboardHeader = () => {
   const showDropdown = isOpen && debouncedSearch !== "";
 
   return (
-    <header className="border-b z-50">
-      <div className="flex h-14 items-center gap-2 px-3 sm:h-16 sm:gap-4 sm:px-6 fixed top-0 w-full bg-background">
+    <header className="border-b">
+      <div className="flex h-14 items-center gap-2 px-3 sm:h-16 sm:gap-4 sm:px-6 fixed top-0 w-full bg-background pl-10 md:pl-6">
         <div className="flex items-center gap-2">
           <Layout className="w-6 h-6 sm:w-8 sm:h-8 text-blue-600 dark:text-blue-400" />
           <span className="font-bold text-lg sm:text-xl text-gray-900 dark:text-white">
